765c5d722fba93aa517367daf75b70c11c857a95
Sweanhuns <65281282+sweanhuns@users.noreply.github.com>
2020-05-19 10:22:42 +1000

    Initial commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speedEnemy = 2f;
+    public GameObject player;
+    public Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = new Vector2(player.transform.position.x, player.transform.position.y);
+        float step = speedEnemy * Time.fixedDeltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+    }
+}
---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public float time = 0.0f;
+    public float interpolationPeriod = 1.0f;
+    public GameObject enemyPrefab;
+    public Vector2 spawnPointUp;
+    public Vector2 spawnPointDown;
+    public float check = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        spawnPointUp = new Vector2(Random.Range(-4.5f, 4.5f), Random.Range(2f, 4.5f));
+        spawnPointDown = new Vector2(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, -2f));
+        time += Time.deltaTime;
+        if (time >= interpolationPeriod)
+        {
+            time = 0.0f;
+            if (check == 1)
+            {
+                GameObject enemyUp = Instantiate(enemyPrefab, spawnPointUp, Quaternion.identity);
+                check = 0;
+            }
+            else if (check == 0)
+            {
+                GameObject enemyDown = Instantiate(enemyPrefab, spawnPointDown, Quaternion.identity);
+                check = 1;
+            }
+        }
+        
+    }
+
+}
